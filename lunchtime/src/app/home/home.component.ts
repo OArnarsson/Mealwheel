0593cb4562632c17e893f5a309addf2c2692688a
OArnarsson <orriarnars@gmail.com>
2017-06-28 17:50:23 +0000

    cleanup

--- lunchtime/src/app/home/home.component.ts
+++ lunchtime/src/app/home/home.component.ts
@@ -19,6 +19,7 @@ export class HomeComponent {
   foodCharts: FoodChart[] = [this.activeChart];
 
   constructor() {
+    // Hamraborg default?
     this.activeChart.foodOptions.push((new FoodOption('Saffran', 1, 'rgba(255, 155, 0, .45)',  'rgba(255, 155, 0, .7)')));
     this.activeChart.foodOptions.push((new FoodOption('Dominos', 1, 'rgba(0, 109, 255, .45)',  'rgba(0, 109, 255, .7)')));
     this.activeChart.foodOptions.push((new FoodOption('Wok on',  1, 'rgba(204, 0, 0, .45)',    'rgba(204,0,0, .7)')));
@@ -45,36 +46,34 @@ export class HomeComponent {
   updateOptionValue(option: FoodOption, increase: true) {
     if (increase) option.value += 1;
     else if (option.value > 0) option.value -= 1;
-
     this.updateData(this.activeChart);
     this.chart.chart.update();
   }
 
   updateData(chart: FoodChart) {
-    console.log('chart: ', chart);
     chart.pieLabels = chart.foodOptions.map(option => option.name);
     chart.pieData = chart.foodOptions.map(option => option.value);
     chart.pieColors[0] = {backgroundColor: chart.foodOptions.map(option => option.color)};
     chart.pieColors[1] = {hoverBackgroundColor: chart.foodOptions.map(option => option.hoverColor)};
-    console.log('after: ', chart);
     this.activeChart = chart;
   }
 
   makeNewChart() {
     let newChart: FoodChart = new FoodChart();
-    newChart.foodOptions.push((new FoodOption('Dominos',  1, 'rgba(0, 109, 255, .45)',  'rgba(0, 109, 255, .7)')));
-    newChart.foodOptions.push((new FoodOption('DJ Grill', 1, 'rgba(204, 0, 0, .45)',    'rgba(204,0,0, .7)')));
-    newChart.foodOptions.push((new FoodOption('Serrano',  1, 'rgba(165, 90, 30, .45)',  'rgba(165, 90, 30, .7)')));
-    newChart.foodOptions.push((new FoodOption('Subway',   1, 'rgba(0, 255, 150, .45)',  'rgba(0, 255, 150, .7)')));
-    newChart.foodOptions.push((new FoodOption('Bónus',    1, 'rgba(255, 255, 0, .45)',  'rgba(255,255,0, .7)')));
-    newChart.foodOptions.push((new FoodOption('Sushi',    1, 'rgba(225, 225, 225, .45)',  'rgba(225,225,225, .7)')));
-    newChart.foodOptions.push((new FoodOption('Nova',     1, 'rgba(196, 24, 255, .45)', 'rgba(196, 24, 255, .7)')));
-    newChart.foodOptions.push((new FoodOption('Greifinn', 1, 'rgba(33, 33, 33, .45)', 'rgba(33, 33, 33, .7)')));
+
+    // Akrueyrar default?
+    newChart.foodOptions.push((new FoodOption('Sushi',    1, 'rgba(225, 225, 225, .45)', 'rgba(225,225,225, .7)')));
+    newChart.foodOptions.push((new FoodOption('Nova',     1, 'rgba(196, 24, 255, .45)',  'rgba(196, 24, 255, .7)')));
+    newChart.foodOptions.push((new FoodOption('Serrano',  1, 'rgba(165, 90, 30, .45)',   'rgba(165, 90, 30, .7)')));
+    newChart.foodOptions.push((new FoodOption('Bónus',    1, 'rgba(255, 255, 0, .45)',   'rgba(255,255,0, .7)')));
+    newChart.foodOptions.push((new FoodOption('DJ Grill', 1, 'rgba(204, 0, 0, .45)',     'rgba(204,0,0, .7)')));
+    newChart.foodOptions.push((new FoodOption('Subway',   1, 'rgba(0, 255, 150, .45)',   'rgba(0, 255, 150, .7)')));
+    newChart.foodOptions.push((new FoodOption('Dominos',  1, 'rgba(0, 109, 255, .45)',   'rgba(0, 109, 255, .7)')));
+    newChart.foodOptions.push((new FoodOption('Greifinn', 1, 'rgba(33, 33, 33, .45)',    'rgba(33, 33, 33, .7)')));
     
     this.foodCharts.push(newChart);
     this.updateData(newChart);
-    setTimeout(() => {this.chart.refresh();}, 1)
-    
+    setTimeout(() => { this.chart.refresh(); }, 1) // Prevents chartjs crash
   }
 
   switchChart(increase: boolean) {
@@ -88,6 +87,6 @@ export class HomeComponent {
         this.updateData(this.foodCharts[this.foodCharts.indexOf(this.activeChart) - 1]);
       } else this.updateData(this.foodCharts[this.foodCharts.length - 1]);
     }
-    setTimeout(() => {this.chart.refresh();}, 1)
+    setTimeout(() => { this.chart.refresh(); }, 1) // Prevents chartjs crash
   } 
 }
