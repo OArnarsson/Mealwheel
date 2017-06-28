e896b64b93a18fa341604ab54c90f4e963a339f1
OArnarsson <orriarnars@gmail.com>
2017-06-28 16:20:55 +0000

    foodChart model added

--- lunchtime/src/app/home/home.component.ts
+++ lunchtime/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import {Component, ViewChild} from '@angular/core';
 import {FoodOption} from "../models/foodOption";
+import {FoodChart} from "../models/foodChart";
 import {BaseChartDirective} from "ng2-charts";
 import '../../../node_modules/chart.piecelabel.js/src/Chart.PieceLabel.js';
 
@@ -14,40 +15,18 @@ export class HomeComponent {
   public chart: BaseChartDirective;
   options: FoodOption[] = [];
   readyToSpin: boolean = true;
-
-  pieColors: any[] = [];
-  pieData: number[] = [];
-  pieLabels: string[] = [];
-  pieType: string = 'pie';
-  pieOptions: any =
-    {
-        responsive: true,
-        legend: {
-          display: false
-        },
-      pieceLabel: {
-        mode: 'label',
-        arc: true,
-        position: 'border',
-        fontSize: 18,
-        fontStyle: 'bold',
-        fontColor: '#ddd',
-        fontFamily: '"Oxygen", cursive',
-      },
-        tooltips: {
-          enabled: false
-        }
-    };
+  foodChart: FoodChart = new FoodChart();
+    
 
   constructor() {
-    this.options.push((new FoodOption('Saffran', 1, 'rgba(255, 155, 0, .45)',  'rgba(255, 155, 0, .7)')));
-    this.options.push((new FoodOption('Dominos', 1, 'rgba(0, 109, 255, .45)',  'rgba(0, 109, 255, .7)')));
-    this.options.push((new FoodOption('Wok on',  1, 'rgba(204, 0, 0, .45)',    'rgba(204,0,0, .7)')));
-    this.options.push((new FoodOption('Serrano', 1, 'rgba(165, 90, 30, .45)',  'rgba(165, 90, 30, .7)')));
-    this.options.push((new FoodOption('Subway',  1, 'rgba(0, 255, 150, .45)',  'rgba(0, 255, 150, .7)')));
-    this.options.push((new FoodOption('Búllan',  1, 'rgba(255, 0, 0, .45)',    'rgba(255,0,0, .7)')));
-    this.options.push((new FoodOption('Krónan',  1, 'rgba(255, 255, 0, .45)',  'rgba(255,255,0, .7)')));
-    this.options.push((new FoodOption('Nova',    1, 'rgba(196, 24, 255, .45)', 'rgba(196, 24, 255, .7)')));
+    this.foodChart.foodOptions.push((new FoodOption('Saffran', 1, 'rgba(255, 155, 0, .45)',  'rgba(255, 155, 0, .7)')));
+    this.foodChart.foodOptions.push((new FoodOption('Dominos', 1, 'rgba(0, 109, 255, .45)',  'rgba(0, 109, 255, .7)')));
+    this.foodChart.foodOptions.push((new FoodOption('Wok on',  1, 'rgba(204, 0, 0, .45)',    'rgba(204,0,0, .7)')));
+    this.foodChart.foodOptions.push((new FoodOption('Serrano', 1, 'rgba(165, 90, 30, .45)',  'rgba(165, 90, 30, .7)')));
+    this.foodChart.foodOptions.push((new FoodOption('Subway',  1, 'rgba(0, 255, 150, .45)',  'rgba(0, 255, 150, .7)')));
+    this.foodChart.foodOptions.push((new FoodOption('Búllan',  1, 'rgba(255, 0, 0, .45)',    'rgba(255,0,0, .7)')));
+    this.foodChart.foodOptions.push((new FoodOption('Krónan',  1, 'rgba(255, 255, 0, .45)',  'rgba(255,255,0, .7)')));
+    this.foodChart.foodOptions.push((new FoodOption('Nova',    1, 'rgba(196, 24, 255, .45)', 'rgba(196, 24, 255, .7)')));
     this.updateData();
   }
 
@@ -72,9 +51,10 @@ export class HomeComponent {
   }
 
   updateData() {
-    this.pieLabels = this.options.map(option => option.name);
-    this.pieData = this.options.map(option => option.value);
-    this.pieColors[0] = {backgroundColor: this.options.map(option => option.color)};
-    this.pieColors[1] = {hoverBackgroundColor: this.options.map(option => option.hoverColor)};
+    this.foodChart.pieLabels = this.foodChart.foodOptions.map(option => option.name);
+    this.foodChart.pieData = this.foodChart.foodOptions.map(option => option.value);
+    this.foodChart.pieData = this.foodChart.foodOptions.map(option => option.value);
+    this.foodChart.pieColors[0] = {backgroundColor: this.foodChart.foodOptions.map(option => option.color)};
+    this.foodChart.pieColors[1] = {hoverBackgroundColor: this.foodChart.foodOptions.map(option => option.hoverColor)};
   }
 }
